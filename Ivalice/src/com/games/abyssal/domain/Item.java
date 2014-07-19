d689e2abf0dadf06bc43f642434c5cc143b9b2d8
alex <alex@alex-PC>
2014-07-19 22:30:29 +0200

    cococomit

--- Ivalice/src/com/games/abyssal/domain/Item.java
+++ Ivalice/src/com/games/abyssal/domain/Item.java
@@ -1,5 +1,13 @@
 package com.games.abyssal.domain;
 
+/**
+ * @author alex
+ *
+ */
 public class Item {
-
+	
+	/** 
+	 * The id.
+	 * */
+	private long id;
 }
---
+++ Ivalice/src/com/games/abyssal/tests/MarketTests.java
@@ -0,0 +1,14 @@
+package com.games.abyssal.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MarketTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
